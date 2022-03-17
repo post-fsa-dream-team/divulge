d3fe1f4a7815734c7336673413a0beb4b2c9e4df
Andrea Khan <khan.andrea@gmail.com>
2022-03-17 12:00:03 -0400

    Home view updates

--- app/views/Home.js
+++ app/views/Home.js
@@ -1,6 +1,8 @@
 import AbstractView from "./AbstractView.js";
 import AllPostNavigation from "./AllPostNavigation.js";
 
+
+
 export default class extends AbstractView {
   constructor(params) {
     super(params)
@@ -8,6 +10,36 @@ export default class extends AbstractView {
   }
 
   async getHtml() {
-    return `<h1 id="home-title">Welcome to Divulge</h1>`
+    // let getPosts = async () => {
+    //   const options = {
+    //      method: 'GET',
+    //      headers: {
+    //          'Content-Type': 'application/json'
+    //      }
+    //  };
+    //  try {
+    //      const response = await fetch("/posts")
+    //      const json = await response.json();
+    //      // console.log(json)
+    //      return json
+    //  } catch (err) {
+    //      console.log('Error getting documents', err)
+    //  }
+    // }
+
+    let myPosts = [{title: "post1", imageUrl: " https://cf.ltkcdn.net/dogs/images/orig/235430-2000x1332-australian-shepherd-puppy.jpg", content:"This is fake content"}, {title: "post2", imageUrl: "https://cf.ltkcdn.net/dogs/images/orig/235430-2000x1332-australian-shepherd-puppy.jpg", content:"This is fake content"}, {title: "post3", imageUrl: "https://cf.ltkcdn.net/dogs/images/orig/235430-2000x1332-australian-shepherd-puppy.jpg", content:"This is fake content"}]
+
+    return `
+      <h1 id="home-title">Welcome to Divulge</h1>
+        <table id="all-posts-table">
+            ${myPosts.map(post =>
+              `<tr>
+                <td class="image-cell"><img src=${post.imageUrl}/></td>
+                <td>${post.title}</td>
+                <td>${post.content}</td>
+              </tr>`
+                ).join('')}
+        </table>
+      `
   }
 }
