e21c4e5da51e3d673821619092e94f201404ec09
Trang Nguyen <ntttrang2130@gmail.com>
2022-03-21 16:00:06 -0400

    add auth tasks

--- server/api/auth.js
+++ server/api/auth.js
@@ -2,38 +2,40 @@ const router = require('express').Router();
 const { pool } = require('../db');
 
 /*********SIGN IN, SIGN UP ************/
-
+/**For signup, we need to check if the user exists
+   - No users found: create new user
+   - Found existing email: login
+ */
 router.post('/signup', async (req, res) => {
-  console.log('req.body', req.body
-  );
-  const { user_name, first_name, last_name, email, password, birth_date, location } = req.body
-
-  const newUser = await pool.query('INSERT INTO users (user_name, first_name, last_name, email, password, birth_date, location) VALUES ($1, $2, $3, $4, $5, $6, $7)', [user_name, first_name, last_name, email, password, birth_date, location], (error, results) => {
-    if (error) {
-      throw error
-    }
-    console.log('results', results);
-    if (req.body.email)
-    response.status(201).send(`User added with ID: ${results}`)
-  })
-})
-
-router.post('./signin', async (req, res) => {
   try {
-    // const user = await User.findOne({ where: { email: req.body.email } })
-    // if (!user) {
-    //   console.log('No such user found:', req.body.email)
-    //   res.status(401).send('Wrong username and/or password')
-    // } else if (!user.correctPassword(req.body.password)) {
-    //   console.log('Incorrect password for user:', req.body.email)
-    //   res.status(401).send('Wrong username and/or password')
-    // } else {
-    //   await Cart.update(
-    //     { userId: user.id },
-    //     { where: { sessionId: req.session.id } }
-    //   )
-    //   req.login(user, err => (err ? next(err) : res.json(user)))
-    // }
+    console.log('req.body', req.body);
+    const { user_name, first_name, last_name, email, password, birth_date, location } = req.body
+
+    const newUser = await pool.query('INSERT INTO users (user_name, first_name, last_name, email, password, birth_date, location) VALUES ($1, $2, $3, $4, $5, $6, $7)', [user_name, first_name, last_name, email, password, birth_date, location], (error, results) => {
+      if (error) throw error
+      if (req.body.email) res.status(201).send('User Signed Up!')
+      console.log('results', results);
+    })
+    /**Check if newUser is in the valid data ? send newUser : console.log('error msg') */
+    newUser ? res.json(newUser) : console.log('Need more information from this user');
+  } catch (error) {
+    console.log(error);
+    next(error)
+  }
+})
+/**For Sign In, we need to:
+  - Find the user email
+  - No users:
+        console.log('No such user found:', req.body.email)
+        res.status(401).send('Wrong username and/or password')
+  - Password is not correct:
+        console.log('Incorrect password for user:', req.body.email)
+        res.status(401).send('Wrong username and/or password')
+  - User found:  req.login(user, err => (err ? next(err) : res.json(user)))
+ */
+router.post('/signin', async (req, res) => {
+  try {
+    const user = await pool.query('')
   } catch (error) {
     console.log(error);
     next(error)
