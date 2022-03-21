be00da51fd19f0c424ed59aa5445df8c73a12d83
Trang Nguyen <ntttrang2130@gmail.com>
2022-03-21 01:03:34 -0400

    add route for single user

--- server/api/users.js
+++ server/api/users.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { pool } = require('../db');
 
-
+//All users
 router.get('/', async (req, res, next) => {
   try {
     let userData = await pool.query('SELECT * FROM users');
@@ -13,5 +13,37 @@ router.get('/', async (req, res, next) => {
     next(error);
   }
 });
+
+//My Profile
+router.get('/:userId', async (req, res) => {
+  try {
+    const { userId } = req.params
+    const { rows } = await pool.query('SELECT * FROM users WHERE id = $1', [userId])
+    res.send(rows[0])
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+})
+/*****************ATTEMPT********** */
+//My Posts
+router.get('/:userId/posts', async (req, res) => {
+  try {
+
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+})
+
+//My Single Post
+router.get('/:userId/posts/:postId', async (req, res) => {
+  try {
+
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+})
 
 module.exports = router;
