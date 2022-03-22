6d9e4b09e5a63796a6c6098f632d2fe1767bf24a
Andrea Khan <khan.andrea@gmail.com>
2022-03-22 08:07:27 -0400

    get post by ID route returning empty response

--- server/api/posts.js
+++ server/api/posts.js
@@ -27,8 +27,9 @@ router.post('/', async(req, res, next) => {
 
 router.get('/:id', async(req, res, next) => {
   try {
-      const response = await pool.query(`SELECT p.id, p.title, p.image_url, p.content, p.category, p.created_at, p.user_id, u.user_name, u.id, u.first_name, u.last_name from POSTS p WHERE id = ${req.params.id} LEFT JOIN users u on p.user_id = u.id`)
-      res.status(200).json(response.rows)
+    let id = req.params.id
+    const response = await pool.query(`SELECT p.id, p.title, p.image_url, p.content, p.category, p.created_at, p.user_id, u.user_name, u.id, u.first_name, u.last_name from POSTS p LEFT JOIN users u on p.user_id = u.id WHERE p.id = ${id}`)
+    res.status(200).json(response)
   } catch (error) {
       console.log(error)
       next(error)
