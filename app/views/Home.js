e9dcf445b5409bdf875c0ead96a3ad6f41fc8404
Andrea Khan <khan.andrea@gmail.com>
2022-03-22 16:08:54 -0400

    updates to the formatting for Home

--- app/views/Home.js
+++ app/views/Home.js
@@ -1,5 +1,4 @@
 import AbstractView from "./AbstractView.js";
-import AllPostsView from "./HomeViews/AllPostsView.js"
 
 let dummyData = [{title: "post1", imageUrl: "https://www.petmd.com/sites/default/files/styles/article_image/public/petmd-shaking-puppy.jpg?itok=4_quJCAy", content:"This is fake content"}, {title: "post2", imageUrl: "https://www.petmd.com/sites/default/files/styles/article_image/public/petmd-shaking-puppy.jpg?itok=4_quJCAy", content:"This is fake content"}, {title: "post3", imageUrl: "https://www.petmd.com/sites/default/files/styles/article_image/public/petmd-shaking-puppy.jpg?itok=4_quJCAy", content:"This is fake content"}]
 
@@ -8,7 +7,7 @@ export default class extends AbstractView {
     super(params)
     this.setTitle("Home");
     this.state = {
-      selectedPosts: "all"
+      selectedPosts: "news"
     }
   }
 
@@ -32,41 +31,40 @@ export default class extends AbstractView {
 
   async getHtml() {
     let posts = await this.getData()
-    if (this.state.selectedPosts === "all") {
-      return AllPostsView(posts)
+
+    if (this.state.selectedPosts !== "all") {
+      posts = this.filterPosts(posts, this.state.selectedPosts)
+      // return AllPostsView(posts)
     }
 
-    else {
-      let currentPosts = this.filterPosts(posts, this.state.selectedPosts)
-      return AllPostsView(currentPosts)
-    }
+    return `
+      <nav class="home-nav">
+        <div id="categories-title">Categories</div>
+        <a href="" class="home-nav-link" data-link>Technology</a>
+        <a href="" class="home-nav-link" data-link>Politics</a>
+        <a href="" class="home-nav-link" data-link>Fashion</a>
+      </nav>
 
-    // return `
-    //   <nav class="home-nav">
-    //     <div id="categories-title">Categories</div>
-    //     <a href="" class="home-nav-link" data-link>Technology</a>
-    //     <a href="" class="home-nav-link" data-link>Politics</a>
-    //     <a href="" class="home-nav-link" data-link>Fashion</a>
-    //   </nav>
-
-    //   <div class="home-content">
-    //     <h1 id="home-title">Welcome to Divulge</h1>
-    //       <table id="all-posts-table">
-    //         <tbody>
-    //           ${posts.map((item) =>
-    //             `<tr>
-    //               <td>
-    //               <div class="article-title">${item.title}</div>
-    //               <div class="author">by ${item.user_name}</div>
-    //               <div>${item.content.slice(0, 200).split(" ").join(" ") + "..."}</div>
-    //               </td>
-    //               <td>
-    //               <img class="post-image" src="${item.image_url}"/>
-    //               </td>
-    //             </tr>`
-    //           ).join('')}
-    //         </tbody>
-    //       </table>
-    //     <div>`
+      <div class="home-content">
+        <h1 id="home-title">Welcome to Divulge</h1>
+          <table id="all-posts-table">
+            <tbody>
+              ${posts.map((item) =>
+                `<tr>
+                  <td>
+                  <div class="article-title">${item.title}</div>
+                  <div class="author">by ${item.user_name}</div>
+                  <div>${item.content.slice(0, 200).split(" ").join(" ") + "..."}</div>
+                  </td>
+                  <td>
+                  <img class="post-image" src="${item.image_url}"/>
+                  </td>
+                </tr>`
+              ).join('')}
+            </tbody>
+          </table>
+        <div>`
   }
+
+  async postRender() {}
 }
