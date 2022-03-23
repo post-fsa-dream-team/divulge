e32557ed496948da033af219da8eb7132784d6d1
Jorgelly Ventura <jorgelly87@gmail.com>
2022-03-23 14:23:06 -0400

    adding text editor funcvtionality

--- app/views/CreatePost.js
+++ app/views/CreatePost.js
@@ -30,62 +30,123 @@ export default class extends AbstractView {
   async getHtml() {
 
     return (`
-      <div>
+      <div id="create-post-container">
         <nav class="home-nav">
-        <div id="categories-title">Categories</div>
-        <a href="" class="home-nav-link" data-link>Technology</a>
-        <a href="" class="home-nav-link" data-link>Politics</a>
-        <a href="" class="home-nav-link" data-link>Fashion</a>
-      </nav>
+          <div id="categories-title">Categories</div>
+          <a href="" class="home-nav-link" data-link>Technology</a>
+          <a href="" class="home-nav-link" data-link>Politics</a>
+          <a href="" class="home-nav-link" data-link>Fashion</a>
+        </nav>
 
-      <div class="home-content">
-        <h1 id="home-title">Welcome to Divulge</h1>
-        <form id="postForm">
+        <div class="home-content">
+          <h1 id="createPost-title">Create Your Content</h1>
           <div>
-            <label for="postTitle">Title: </label>
-            <input id="post-title" type="text" name="postTitle" placeholder="Enter a title" value="" />
+            <div>
+              <!--<label id="label-title">Title: </label>-->
+              <input id="input-title" class="inputField" type="text" name="title" placeholder="Title" value="" />
+            </div><br />
+            <div>
+              <!--<label id="label-imageUrl">Title Image URL: </label>-->
+              <input id="input-imageUrl" class="inputField" type="text" name="imageUrl" placeholder="Thumbnail URL" value="" />
+              <!-- <iframe id="input-imageUrl" class="inputField" name="imageURL-field" placeholder="Enter the image url for the title" value=""></iframe> -->
+            </div><br />
+            <form id="text-editor-form">
+              <button type="button" data-cmd="justifyLeft">
+                <i class="fa fa-align-left" aria-hidden="true"></i>
+              </button>
+              <button type="button" data-cmd="justifyCenter">
+                <i class="fa fa-align-center" aria-hidden="true"></i>
+              </button>
+              <button type="button" data-cmd="justifyFull">
+                <i class="fa fa-align-justify" aria-hidden="true"></i>
+              </button>
+              <button type="button" data-cmd="justifyRight">
+                <i class="fa fa-align-right" aria-hidden="true"></i>
+              </button>
+              <button type="button" data-cmd="bold">
+                <i class="fa fa-bold" aria-hidden="true"></i>
+              </button>
+              <button type="button" data-cmd="italic">
+                <i class="fa fa-italic" aria-hidden="true"></i>
+              </button>
+              <button type="button" data-cmd="underline">
+                <i class="fa fa-underline" aria-hidden="true"></i>
+              </button>
+              <button type="button" data-cmd="textHeight">
+                <i class="fa fa-text-height" aria-hidden="true"></i>
+              </button>
+              <button type="button" data-cmd="insertOrderedList">
+                <i class="fa fa-list-ol" aria-hidden="true"></i>
+              </button>
+              <button type="button" data-cmd="insertUnorderedList">
+                <i class="fa fa-list-ul" aria-hidden="true"></i>
+              </button>
+              <button type="button" data-cmd="insertImage">
+                <i class="fa fa-file-image-o" aria-hidden="true"></i>
+              </button>
+              <button type="button" data-cmd="createLink">
+                <i class="fa fa-link" aria-hidden="true"></i>
+              </button>
+              <button type="button" data-cmd="showCode" name="active">
+                <i class="fa fa-code" aria-hidden="true"></i>
+              </button>
+            </form>
+            <div>
+              <iframe id="output" name="textField" value=""></iframe>
+            </div>
+            <br />
+            <div>
+              <!--<label id="label-category">Category: </label>-->
+              <select id="post-category" name="category">
+                <option value="none" selected disabled hidden>Select a Category</option>
+                <option value="technology">Technology</option>
+                <option value="politics">Politics</option>
+                <option value="fashion">Fashion</option>
+              </select>
+            </div>
           </div>
-          <div>
-            <label for="postContent">Content: </label>
-            <input id="post-content" type="text" name="postContent" placeholder="Enter content" value="" />
-          </div>
-          <div>
-            <label for="postCategory">Category: </label>
-            <input id="post-category" type="text" name="postCategory" placeholder="ie. technology, entertainment, etc." value="" />
-          </div>
-          <div>
-            <label for="postImage">Image URL: </label>
-            <input id="post-imageURL" type="text" name="postImage" placeholder="Enter Image URL" value="" />
-          </div>
-          <button type="submit">Submit Post</button>
-        </form>
-      </div>
+        </div>
       </div>
 
     `);
   }
 
   async postRender() {
-    const createPost = this.createPost;
-    document.getElementById("postForm").addEventListener("submit", async function(e) {
-      e.preventDefault();
-      const title = document.getElementById('post-title');
-      const content = document.getElementById('post-content');
-      const category = document.getElementById('post-category');
-      const image_url = document.getElementById('post-imageURL');
-      const post = {
-        title: title.value,
-        content: content.value,
-        category: category.value,
-        image_url: image_url.value,
-        user_id: 2
-      }
-      if (category.value.trim() === "") {
-        alert('Please fill in category');
-        category.focus();
-        return;
-      }
-      createPost(post);
-    });
+    // const createPost = this.createPost;
+    // document.getElementById("postForm").addEventListener("submit", async function(e) {
+    //   e.preventDefault();
+    //   const title = document.getElementById('post-title');
+    //   const content = document.getElementById('post-content');
+    //   const category = document.getElementById('post-category');
+    //   const image_url = document.getElementById('post-imageURL');
+    //   const post = {
+    //     title: title.value,
+    //     content: content.value,
+    //     category: category.value,
+    //     image_url: image_url.value,
+    //     user_id: 2
+    //   }
+    //   if (category.value.trim() === "") {
+    //     alert('Please fill in category');
+    //     category.focus();
+    //     return;
+    //   }
+    //   createPost(post);
+    // });
+    const buttons = document.querySelectorAll('button');
+    textField.document.designMode = 'On';
+
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener('click', () => {
+        let cmd = buttons[i].getAttribute('data-cmd');
+        if (buttons[i].name === 'active') buttons[i].classList.toggle('active');
+        if (cmd === 'insertImage' || cmd === 'createLink') {
+          let url = prompt('Enter Link here: ', '');
+          textField.document.execCommand(cmd, false, url);
+        } else {
+          textField.document.execCommand(cmd, false, null);
+        }
+      })
+    }
   }
 }
