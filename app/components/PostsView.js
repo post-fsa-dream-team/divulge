6a10b00aa2acbef85fcbc4e2bb1a3395cf4d464e
Andrea Khan <khan.andrea@gmail.com>
2022-03-24 14:28:41 -0400

    grid updates for posts; added category

--- app/components/PostsView.js
+++ app/components/PostsView.js
@@ -1,12 +1,10 @@
 
 const PostsView = (posts) => {
   function convertDate(date) {
-    console.log(date.split(''))
     let splitIndex = date.split("").indexOf("T")
-    console.log(splitIndex+1)
     let dayOnly = date.slice(0, splitIndex)
-    console.log(dayOnly)
-    return new Date(dayOnly)
+    let justDate = new Date(dayOnly).toString()
+    return justDate.split(" ").slice(0, 4).join(" ")
   }
 
   return `
@@ -18,11 +16,13 @@ const PostsView = (posts) => {
         <a class="article-link" href="/posts/${item.id}">
         <div id="cards-container">
           <div class="authorAndCreated">
-            <span class="article-date">${convertDate(item.created_at)}</span><span class="author">by ${item.user_name}</span>
-            <div class="article-title">${item.title}</div>
-          </div>
+          <span class="author"> ${item.user_name}</span><span class="article-date"> • ${convertDate(item.created_at)}</span>
+            </div>
+          <div class="article-title">${item.title}</div>
           <img class="post-image" src="${item.image_url}"/>
           <div class="article-content">${item.content.slice(0, 200).split(" ").join(" ") + "..."}</div>
+
+          <div class="article-category"><span class="category-border">${item.category}</span></div>
           </div>
           </a>
           </div>`
