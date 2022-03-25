13ec517a949ba5cf66f5a8d57dc0bb14de083ca1
Andrea Khan <khan.andrea@gmail.com>
2022-03-25 18:09:56 -0400

    updating SideNav with categories

--- app/components/SideNav.js
+++ app/components/SideNav.js
@@ -4,8 +4,9 @@ const SideNav = () => {
     <a href="/posts/all/all" id="all-posts-link" data-link><span>All Posts</span></a>
     <div id="categories">Posts By Category</div>
     <a href="/posts/all/news" class="home-nav-link" data-link>News</a>
-    <a href="/posts/all/crime" class="home-nav-link" data-link>Crime</a>
     <a href="/posts/all/technology" class="home-nav-link" data-link>Technology</a>
+    <a href="/posts/all/politics" class="home-nav-link" data-link>Politics</a>
+    <a href="/posts/all/crime" class="home-nav-link" data-link>Crime</a>
     <a href="/posts/all/entertainment" class="home-nav-link" data-link>Entertainment</a>
     <a href="/posts/all/fashion" class="home-nav-link" data-link>Fashion</a>
     <a href="/posts/all/sports" class="home-nav-link" data-link>Sports</a>
