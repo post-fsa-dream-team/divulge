13c6abd488c6b7c609fc74b5e6105a6cc69db0e5
Andrea Khan <khan.andrea@gmail.com>
2022-03-24 20:58:36 -0400

    other single post view styling updates

--- app/views/OtherSinglePost.js
+++ app/views/OtherSinglePost.js
@@ -22,10 +22,26 @@ export default class extends AbstractView {
   async getHtml() {
     const postArr = await this.getData(this.postId)
     let post = postArr[0]
+
+    function convertDate(date) {
+      let splitIndex = date.split("").indexOf("T")
+      let dayOnly = date.slice(0, splitIndex)
+      let justDate = new Date(dayOnly).toString()
+      return justDate.split(" ").slice(1, 4).join(" ")
+    }
+
+    function getMinRead(content) {
+      return Math.ceil(content.length / 500)
+    }
+
     return `
     ${Navbar()}
     ${SideNav()}
       <div class='single-other-post-container'>
+        <div class="single-other-date-and-read">
+          <span class="single-other-post-date">${convertDate(post.created_at)}</span>
+          <span class="single-other-post-min-read"> • ${getMinRead(post.content)} Min Read</span>
+        </div>
         <div class="single-other-post-title">${post.title}</div>
         <div class="single-other-post-username">${post.user_name}</div>
         <img class="single-other-post-img" src="${post.image_url}"/>
