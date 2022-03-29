4673717104eb018a69837a58622c038c0f29795c
Trang Nguyen <ntttrang2130@gmail.com>
2022-03-29 14:34:46 -0400

    merge conflicts

--- app/views/SignUp.js
+++ app/views/SignUp.js
@@ -21,7 +21,7 @@ export default class extends AbstractView {
                 <p> <strong>Try it free 7 days</strong> then $20/mo. thereafter </p>
             </div>
 
-        <form class='signup__form' id='form'>
+        <form class='signup__form' id='signup__form'>
         <div class='signup__form-control'>
             <input type="text" id="firstname" placeholder="First Name"/>
             <small>First Name cannot be empty</small>
@@ -39,7 +39,7 @@ export default class extends AbstractView {
             <small>Password cannot be empty</small>
         </div>
 
-        <button style="text-transform:uppercase">Sign up and claim your free trial</button>
+        <button style="text-transform:uppercase" >Sign up and claim your free trial</button>
         <small>By clicking the button, you are agreeing to our <a href="href">Terms and Services</a>.</small>
     </form>
     </div>
@@ -49,9 +49,59 @@ export default class extends AbstractView {
     }
 
     async postRender() {
-        const form = document.getElementById('form')
-        form.addEventListener('submit', async (e) => {
+        const form = document.getElementById('signup__form');
+
+        form.addEventListener('submit', e => {
             e.preventDefault();
-        })
+
+            const firstName = form['firstname'].value;
+            const lastName = form['lastname'].value;
+            const email = form['email'].value;
+            const password = form['password'].value;
+
+            if (firstName === '') {
+                addErrorTo('firstname', 'First Name is required');
+            } else {
+                removeErrorFrom('firstname');
+            }
+
+            if (lastName === '') {
+                addErrorTo('lastname', 'Last Name is required');
+            } else {
+                removeErrorFrom('lastname');
+            }
+
+            if (email === '') {
+                addErrorTo('email', 'Email is required');
+            } else if (!isValid(email)) {
+                addErrorTo('email', 'Email is not valid');
+            } else {
+                removeErrorFrom('email');
+            }
+
+            if (password === '') {
+                addErrorTo('password', 'Password is required');
+            } else {
+                removeErrorFrom('password');
+            }
+        });
+
+        function addErrorTo(field, message) {
+            const formControl = form[field].parentNode;
+            formControl.classList.add('error');
+
+            const small = formControl.querySelector('small');
+            small.innerText = message;
+        }
+
+        function removeErrorFrom(field) {
+            const formControl = form[field].parentNode;
+            formControl.classList.remove('error');
+        }
+
+        function isValid(email) {
+            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(String(email).toLowerCase());
+        }
     }
 }
