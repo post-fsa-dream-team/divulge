650922aede7da4d8b54c4231d8929c0f3879c2df
Andrea Khan <khan.andrea@gmail.com>
2022-04-01 12:12:32 -0400

    logout styling

--- app/views/Logout.js
+++ app/views/Logout.js
@@ -15,7 +15,7 @@ export default class extends AbstractView {
       <h1 id="logout-title">Logout</h1>
       <img id="logout-image" alt="stay-image" src="https://media3.giphy.com/media/DADnfSLIFxDUc/giphy.gif" />
       <p id="main-text">Are you sure you'd like to logout?</p>
-      <button id="logout-button">Yes, log me out!</button>
+      <button id="logout-button">Yup, log me out!</button>
       <button id="back-home-button">Nah, return home.</button>
       </div>
       </div>
