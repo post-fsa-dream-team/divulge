cbc00a467e50540a0de63ed5168f5ff92554a4cd
Andrea Khan <khan.andrea@gmail.com>
2022-04-01 12:11:52 -0400

    logout styling

--- app/views/Logout.js
+++ app/views/Logout.js
@@ -9,6 +9,7 @@ export default class extends AbstractView {
   async getHtml() {
     return`
       ${Navbar()}
+      <div id="logout-body">
       <div id="logout-container">
 
       <h1 id="logout-title">Logout</h1>
@@ -16,6 +17,7 @@ export default class extends AbstractView {
       <p id="main-text">Are you sure you'd like to logout?</p>
       <button id="logout-button">Yes, log me out!</button>
       <button id="back-home-button">Nah, return home.</button>
+      </div>
       </div>
       `
   }
