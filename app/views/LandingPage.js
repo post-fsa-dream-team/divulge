4de5dcd41dc2ce08aa750a8118a33d0a530642de
Trang Nguyen <ntttrang2130@gmail.com>
2022-04-11 13:43:15 -0400

    add json data

--- app/views/LandingPage.js
+++ app/views/LandingPage.js
@@ -9,73 +9,90 @@ export default class extends AbstractView {
     this.setTitle("LandingPage");
   }
 
-  async getData() {
-    const response = await fetch("http://localhost:3000/api/posts");
-    const data = await response.json();
-    return data;
+  async getPosts() {
+    try {
+      const data = await fetch("https://jsonplaceholder.typicode.com/posts").then(function (response) {
+        if(response.ok) {
+          return response.json()
+        } else {
+          return Promise.reject(response);
+        };
+      }).then(data => {
+        console.log(data);
+      }).catch(error => {
+        console.log(error);
+      });
+      return data;
+    } catch (error) {
+      console.log('CANNOT SEE DATA', error)
+    }
   }
 
   async getHtml() {
-    console.log(this.state);
-    let posts = await this.getData();
-
+    let posts = await this.getPosts();
+    console.log(posts);
 
     // ${SideNav}
     // ${ Navbar }
+    let navContent = ["Our Story", "Membership", "Write"]
     return `
-    <div class="landingpage__header">
-      <div id="nav">
-        <h1>Divulge</h1>
-        <p>Our Story</p>
-        <p>Membership</p>
-        <p>Write</p>
-        <p>Sign In</p>
-        <p>Sign Up</p>
+    <div class="landingpage">
+      <div class="landingpage__header">
+        <div id="nav">
+          <h1>Divulge</h1>
+          <div id="nav__content">
+            <span>Our Story</span>
+            <span>Membership</span>
+            <span>Write</span>
+            <span>Sign In</span>
+            <span>Sign Up</span>
+          </div>
+        </div>
+        <h1>Stay curious.</h1>
+        <p>Discover stories, thinking and expertise from writers on any topic.</p>
+        <button>Start reading</button>
       </div>
-      <h1>Stay curious.</h1>
-      <p>Discover stories, thinking and expertise from writers on any topic.</p>
-      <button>Start reading</button>
-    </div>
-    <div class="landingpage__short">
-      <p>Trending on Medium</p>
-      <div id="highlight">
-        <p>Story 1</p>
-        <p>Story 2</p>
-        <p>Story 3</p>
-        <p>Story 4</p>
-        <p>Story 5</p>
+      <div class="landingpage__short">
+        <p>Trending on Medium</p>
+        <div id="highlight">
+          <p>Story 1</p>
+          <p>Story 2</p>
+          <p>Story 3</p>
+          <p>Story 4</p>
+          <p>Story 5</p>
+        </div>
       </div>
-    </div>
-    <div class="landingpage__infinitescroll">
-      <div class="landingpage__scrollstory>
-        <p>Writer's name</p>
-        <h1>Title</h1>
-        <h2>This is a summary</h2>
-        <p>MM/YYYY</p>
-        <p>Reading Time</p>
-        <p>tags</p>
-        <p>Save</p>
-        <img/>
-      </div>
-      <div class="landingpage__stickyright">
-        <h1>Discover more of what matters to you</h1>
-        <p>Tag 1</p>
-        <p>Tag 2</p>
-        <p>Tag 3</p>
-        <p>Tag 4</p>
-        <p>Tag 5</p>
-        <p>Tag 6</p>
-        <br>
-        <div class="landingpage__helper">
-          <p>Help</p>
-          <p>Status</p>
-          <p>Writers</p>
-          <p>Blog</p>
-          <p>Careers</p>
-          <p>Privacy</p>
-          <p>Terms</p>
-          <p>About</p>
-          <p>Knowable</p>
+      <div class="landingpage__infinitescroll">
+        <div class="landingpage__scrollstory>
+          <p>Writer's name</p>
+          <h1>Title</h1>
+          <h2>This is a summary</h2>
+          <p>MM/YYYY</p>
+          <p>Reading Time</p>
+          <p>tags</p>
+          <p>Save</p>
+          <img/>
+        </div>
+        <div class="landingpage__stickyright">
+          <h1>Discover more of what matters to you</h1>
+          <p>Tag 1</p>
+          <p>Tag 2</p>
+          <p>Tag 3</p>
+          <p>Tag 4</p>
+          <p>Tag 5</p>
+          <p>Tag 6</p>
+          <br>
+          <div class="landingpage__helper">
+            <p>Help</p>
+            <p>Status</p>
+            <p>Writers</p>
+            <p>Blog</p>
+            <p>Careers</p>
+            <p>Privacy</p>
+            <p>Terms</p>
+            <p>About</p>
+            <p>Knowable</p>
+          </div>
         </div>
       </div>
     </div>
